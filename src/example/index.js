30b602339c5a3ee2ee2072c70412c9b42c39eb8a
Poojan Bedi <poojan.bedi@gmail.com>
2023-08-26 09:32:06 -0700

    Added react-dom with its usage

---
+++ src/example/index.js
@@ -0,0 +1,8 @@
+import {client} from 'react-dom'
+
+const App = () => {
+    return <div>Examples</div>
+}
+
+const mainContainer = document.getElementById('root') | document.getElementsByTagName('body')[0]
+mainContainer.render(<App />)
